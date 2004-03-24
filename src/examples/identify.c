a1bdc76b632a6817ce2372f778b491238d5cf254
conrad <conrad@8158c8cd-e7e1-0310-9fa4-c5954c97daef>
2004-03-24 07:26:31 +0000

    changed copyright attribution to CSIRO
    git-svn-id: http://svn.annodex.net/libfishsound/trunk@224 8158c8cd-e7e1-0310-9fa4-c5954c97daef

--- src/examples/identify.c
+++ src/examples/identify.c
@@ -1,5 +1,6 @@
-/*
-   Copyright (c) 2002, 2003, Xiph.org Foundation
+/**
+   Copyright (C) 2003 Commonwealth Scientific and Industrial Research
+   Organisation (CSIRO) Australia
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
@@ -12,14 +13,14 @@
    notice, this list of conditions and the following disclaimer in the
    documentation and/or other materials provided with the distribution.
 
-   - Neither the name of the Xiph.org Foundation nor the names of its
+   - Neither the name of CSIRO Australia nor the names of its
    contributors may be used to endorse or promote products derived from
    this software without specific prior written permission.
 
    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
    ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+   PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE ORGANISATION OR
    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
