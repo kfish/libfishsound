b9b5c28c18f9a9b42f57197abafcc70f76d43b98
conrad <conrad@8158c8cd-e7e1-0310-9fa4-c5954c97daef>
2005-07-16 06:59:43 +0000

    libfishsound-stable: move ilv inlines to convert.h (backport from changeset:294)
    git-svn-id: http://svn.annodex.net/libfishsound/branches/1.0-stable@1378 8158c8cd-e7e1-0310-9fa4-c5954c97daef

--- src/libfishsound/private.h
+++ src/libfishsound/private.h
@@ -185,36 +185,4 @@ const FishSoundComment * fish_sound_comment_first (FishSound * fsound);
 const FishSoundComment *
 fish_sound_comment_next (FishSound * fsound, const FishSoundComment * comment);
 
-/* inline functions */
-
-static inline void
-_fs_deinterleave (float ** src, float * dest[],
-		  long frames, int channels, float mult_factor)
-{
-  int i, j;
-  float * d, * s = (float *)src;
-
-  for (i = 0; i < frames; i++) {
-    for (j = 0; j < channels; j++) {
-      d = dest[j];
-      d[i] = s[i*channels + j] * mult_factor;
-    }
-  }
-}
-
-static inline void
-_fs_interleave (float * src[], float ** dest,
-		long frames, int channels, float mult_factor)
-{
-  int i, j;
-  float * s, * d = (float *)dest;
-
-  for (i = 0; i < frames; i++) {
-    for (j = 0; j < channels; j++) {
-      s = src[j];
-      d[i*channels + j] = s[i] * mult_factor;
-    }
-  }
-}
-
 #endif /* __FISH_SOUND_PRIVATE_H__ */
