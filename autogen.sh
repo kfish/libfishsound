878f08bf97c7d7f99b395e34d1daa9a72d63e9af
giles <giles@8158c8cd-e7e1-0310-9fa4-c5954c97daef>
2006-10-21 04:50:42 +0000

    Add more recent automake versions to autogen.sh.
    git-svn-id: http://svn.annodex.net/libfishsound/trunk@2531 8158c8cd-e7e1-0310-9fa4-c5954c97daef

--- autogen.sh
+++ autogen.sh
@@ -41,7 +41,11 @@ fi
 #
 
 automake_version="none"
-if automake-1.7 --version >/dev/null 2>&1; then
+if automake-1.9 --version >/dev/null 2>&1; then
+  automake_version="-1.9"
+elif automake-1.8 --version >/dev/null 2>&1; then
+  automake_version="-1.8"
+elif automake-1.7 --version >/dev/null 2>&1; then
   automake_version="-1.7"
 elif automake-1.6 --version >/dev/null 2>&1; then
   automake_version="-1.6"
