e086dd8d154ce9e052fec0c0b31e8312ffe27223
laser13 <laser13@8158c8cd-e7e1-0310-9fa4-c5954c97daef>
2007-06-16 00:08:36 +0000

    Defined by default HAVE_SPEEX_1 and FS_FLOAT in config.h to work with recent versions of libvorbis and libspeex.
    git-svn-id: http://svn.annodex.net/libfishsound/trunk@2940 8158c8cd-e7e1-0310-9fa4-c5954c97daef

--- win32/config.h
+++ win32/config.h
@@ -82,6 +82,12 @@
 /* Version number of package */
 #define VERSION "0.5.41"
 
+/* Define if you have recent version of speex */
+#define HAVE_SPEEX_1_1 1
+
+/* Define FS_FLOAT for recent versions of vorbis */
+#define FS_FLOAT 1
+
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
 /* #undef WORDS_BIGENDIAN */
